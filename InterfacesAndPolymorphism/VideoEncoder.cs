7a36c784875afdec3a1b1199c6c7f0a08bad37f3
mroyski <mroyski@gmail.com>
2019-06-15 14:39:50 -0400

    video relies on interface list

--- InterfacesAndPolymorphism/VideoEncoder.cs
+++ InterfacesAndPolymorphism/VideoEncoder.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfacesAndPolymorphism
 {
     public class VideoEncoder
     {
-        private readonly MailService _mailService;
+        private readonly IList<INotificationChannel> _notificationChannels;
 
         public VideoEncoder()
         {
-            _mailService = new MailService();
+            _notificationChannels = new List<INotificationChannel>();
         }
 
         public void Encode(Video video)
         {
-            _mailService.Send(new Mail());
+            foreach (var channel in _notificationChannels)
+                channel.Send(new Message());
         }
     }
 }
